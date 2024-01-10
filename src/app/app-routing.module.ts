ac81a459af5b683210afb56bcc8d41f402110f8a
faiyaz666 <faiyazrafid5@gmail.com>
2024-01-10 21:44:18 +0600

    pokemon-list updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeeDetailsComponent } from './components/see-details/see-details.component';
+import { PokemonListsComponent } from './components/pokemon-lists/pokemon-lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pokemon-lists',
+    pathMatch: 'full',
+  },
+  {
+    path: 'pokemon-lists',
+    component: PokemonListsComponent,
+  },
+  {
+    path: 'see-details',
+    component: SeeDetailsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'pokemon-lists',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
