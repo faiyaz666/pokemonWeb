8fa2a9d0ee14b3e05ab6ba124178567e2d9c3451
faiyaz666 <faiyazrafid5@gmail.com>
2024-01-10 20:03:15 +0600

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBannerComponent } from './components/top-banner/top-banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PokemonListsComponent } from './components/pokemon-lists/pokemon-lists.component';
+import { SeeDetailsComponent } from './components/see-details/see-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBannerComponent,
+    FooterComponent,
+    PokemonListsComponent,
+    SeeDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pokemon-lists/pokemon-lists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-lists',
+  templateUrl: './pokemon-lists.component.html',
+  styleUrl: './pokemon-lists.component.scss'
+})
+export class PokemonListsComponent {
+
+}
