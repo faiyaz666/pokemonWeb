f7f1d301d0a57b3446020438cc2115de640921ae
faiyaz666 <faiyazrafid5@gmail.com>
2024-01-14 19:42:47 +0600

    pokemon service created

---
+++ src/app/service/poke-api.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, switchMap, forkJoin } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokeApiService {
+  private baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+  constructor(private httpClient: HttpClient) {}
+  getPokemonDetails(pageNo: number): Observable<any> {
+    const url = `${this.baseUrl}?offset=${pageNo * 20}&limit=20`;
+
+    return this.fetchPokemonList(url).pipe(
+      switchMap((result) =>
+        forkJoin(
+          result.results.map((pokemon: any) =>
+            this.fetchIndividualPokemonDetailsByUrl(pokemon.url)
+          )
+        )
+      )
+    );
+  }
+
+  fetchPokemonList(url: string): Observable<any> {
+    return this.httpClient.get<any>(url);
+  }
+
+  fetchIndividualPokemonDetailsByName(name: string): Observable<any> {
+    const url = `${this.baseUrl}/${name}`;
+    return this.fetchIndividualPokemonDetailsByUrl(url);
+  }
+
+  fetchIndividualPokemonDetailsByUrl(url: string): Observable<any> {
+    return this.httpClient.get<any>(url);
+  }
+}
+
